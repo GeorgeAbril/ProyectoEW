8a32d8bcdb60992f77170287a658708623903382
GeorgeAbril <128765495+GeorgeAbril@users.noreply.github.com>
2023-03-31 19:25:54 -0500

    segundo commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './admin/user/login/login.component';
+import { NewRegisterComponent } from './admin/user/new-register/new-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin/user/login', component: LoginComponent },
+  { path: 'admin/user/new', component: NewRegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './admin/user/login/login.component';
+import { NewRegisterComponent } from './admin/user/new-register/new-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NewRegisterComponent
   ],
   imports: [
     BrowserModule,
